2e812e4f7e70e78b7fb7c2400a13b64cedeb3e67
ynoami <noami@fscreator.com>
2017-07-04 16:20:34 +0900

    M_から始まるテーブルをExcelかJSON形式で保存する

---
+++ src/SSTableToExcel/Models/DBSetting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSTableToExcel.Models
+{
+    public class DBSetting
+    {
+        public string サーバー名 { get; set; }
+        public string データベース名 { get; set; }
+        public string ユーザー名 { get; set; }
+        public string パスワード { get; set; }
+    }
+}
--- src/SSTableToExcel/Program.cs
+++ src/SSTableToExcel/Program.cs
@@ -10,6 +10,19 @@ namespace SSTableToExcel
     {
         static void Main(string[] args)
         {
+            Console.Write("PC Name:");
+            var pcName = Console.ReadLine();
+            Console.Write("Database Name:");
+            var dbName = Console.ReadLine();
+            Console.Write("Login Name:");
+            var userName = Console.ReadLine();
+            Console.Write("Password:");
+            var password = Console.ReadLine();
+            
+            var setting = new Models.DBSetting() { サーバー名 = pcName, データベース名 = dbName, ユーザー名 = userName, パスワード = password };
+
+            var db = new Models.DBAccess();
+            db.Getter(setting);
         }
     }
 }
